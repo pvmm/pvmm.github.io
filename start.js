262e5847ff21c7ea4ed7c6af21869ddaabe009b3
Pedro de Medeiros <pedrovmm@gmail.com>
2018-01-31 11:34:27 -0200

    Consertando erro lançado quando arquivo não existe.

--- start.js
+++ start.js
@@ -8,7 +8,12 @@ http.createServer(function (request, response) {
   if (request.url == '/') {
     fs.createReadStream(`${__dirname}/app/index.html`).pipe(response);
   } else {
-    fs.createReadStream(`${__dirname}/app` + request.url).pipe(response);
+    if (fs.existsSync(`${__dirname}/app/${request.url}`)) {
+      fs.createReadStream(`${__dirname}/app` + request.url).pipe(response);
+    } else {
+      console.log(`File not found: ${request.url}`);
+      fs.createReadStream(`${__dirname}/app/index.html`).pipe(response);
+    }
   }
 }).listen(8000);
 
