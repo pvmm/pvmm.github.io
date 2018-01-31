dfcd155db3ab797e6c4781d0597d020f888004c3
Pedro de Medeiros <pedrovmm@gmail.com>
2018-01-31 15:41:52 -0200

    Alterações para executar em node mais antigo

--- start.js
+++ start.js
@@ -1,18 +1,19 @@
 "use strict";
 
-const fs   = require('fs');
-const http = require("http");
+var fs   = require('fs');
+var http = require("http");
+var path = __dirname;
 
 http.createServer(function (request, response) {
   console.log("request.url = ", request.url);
   if (request.url == '/') {
-    fs.createReadStream(`${__dirname}/app/index.html`).pipe(response);
+    fs.createReadStream(path + '/app/index.html').pipe(response);
   } else {
-    if (fs.existsSync(`${__dirname}/app/${request.url}`)) {
-      fs.createReadStream(`${__dirname}/app` + request.url).pipe(response);
+    if (fs.existsSync(path + '/app/' + request.url)) {
+      fs.createReadStream(path + '/app' + request.url).pipe(response);
     } else {
-      console.log(`File not found: ${request.url}`);
-      fs.createReadStream(`${__dirname}/app/index.html`).pipe(response);
+      console.log('File not found: ' + request.url);
+      fs.createReadStream(path + '/app/index.html').pipe(response);
     }
   }
 }).listen(8000);
