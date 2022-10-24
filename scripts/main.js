c77d615fc851601c06b384bbb157eaf163f7738b
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-10-24 17:46:30 -0300

    this is a mess

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-requirejs(["./ace/ace"], function(ace) {
-  var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/twilight");
-  //editor.session.setMode("ace/mode/javascript");
-  editor.session.setMode("ace/mode/c");
-});
