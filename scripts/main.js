f4e45cf7da359fc7e34037f93bbfcb4d885253a1
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-10-24 17:05:33 -0300

    Added requireJs

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+var ace_dir = require("./ace/ace.js");
+var editor = ace_dir.ace.edit("editor");
+editor.setTheme("ace/theme/twilight");
+//editor.session.setMode("ace/mode/javascript");
+editor.session.setMode("ace/mode/c");
